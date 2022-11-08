94d8bd9d0ebab1ff609e22107eebb2a1f6734e4b
adarachel <108772486+adarachel@users.noreply.github.com>
2022-11-08 16:41:32 +0100

    complete ES6

--- modules/books.js
+++ modules/books.js
@@ -1,16 +1,19 @@
+/* eslint-diable */
 class Book {
-    constructor(id, title, author) {
-      this.id = id;
-      this.title = title;
-      this.author = author;
-    }
-  
-    addBook = () => {
-      library.push(this);
-    };
-  
-    removeBook = () => {
-      library = library.filter((book) => book.id !== this.id);
-    };
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
   }
-  export default Book; 
+
+  addBook = () => {
+    // eslint-disable-next-line no-undef
+    library.push(this);
+  };
+  
+  removeBook = () => {
+    // eslint-disable-next-line no-undef
+    library = library.filter((book) => book.id !== this.id);
+  };
+}
+export default Book;
