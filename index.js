e83f6bcb37f90ade72534b22a11b5a61a3d60a6e
adarachel <108772486+adarachel@users.noreply.github.com>
2022-11-08 14:43:22 +0100

    Awesome books: with ES6

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /*eslint-disable*/
 import Book from './modules/books.js';
 import './modules/app.js'
-import './modules/script.js'
 import { DateTime } from './modules/luxon.js';
 
 const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-document.getElementById('todays-date').innerHTML = todaysDate;
+document.getElementById('current-date').innerHTML = currentDate;
